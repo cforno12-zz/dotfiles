6ecadec9718a83f7460440b94d75dc7dfebf02f9
Cris Forno <cforno1@binghamton.edu>
2018-05-24 19:24:31 -0500

    updated atom plugins

---
+++ .atom/compile-cache/coffee/0679f27c0b4f242867900e1feb110f2d107ce4ac.js
@@ -0,0 +1,21 @@
+(function() {
+  module.exports = {
+    name: "Blade",
+    namespace: "blade",
+    fallback: ["html"],
+
+    /*
+    Supported Grammars
+     */
+    grammars: ["Blade"],
+
+    /*
+    Supported extensions
+     */
+    extensions: ["blade.php"],
+    options: []
+  };
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiL1VzZXJzL0NyaXNGb3Juby9kb3RmaWxlcy8uYXRvbS9wYWNrYWdlcy9hdG9tLWJlYXV0aWZ5L3NyYy9sYW5ndWFnZXMvYmxhZGUuY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQUEsTUFBTSxDQUFDLE9BQVAsR0FBaUI7SUFFZixJQUFBLEVBQU0sT0FGUztJQUdmLFNBQUEsRUFBVyxPQUhJO0lBSWYsUUFBQSxFQUFVLENBQUMsTUFBRCxDQUpLOztBQU1mOzs7SUFHQSxRQUFBLEVBQVUsQ0FDUixPQURRLENBVEs7O0FBYWY7OztJQUdBLFVBQUEsRUFBWSxDQUNWLFdBRFUsQ0FoQkc7SUFvQmYsT0FBQSxFQUFTLEVBcEJNOztBQUFqQiIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0ge1xuXG4gIG5hbWU6IFwiQmxhZGVcIlxuICBuYW1lc3BhY2U6IFwiYmxhZGVcIlxuICBmYWxsYmFjazogW1wiaHRtbFwiXVxuXG4gICMjI1xuICBTdXBwb3J0ZWQgR3JhbW1hcnNcbiAgIyMjXG4gIGdyYW1tYXJzOiBbXG4gICAgXCJCbGFkZVwiXG4gIF1cblxuICAjIyNcbiAgU3VwcG9ydGVkIGV4dGVuc2lvbnNcbiAgIyMjXG4gIGV4dGVuc2lvbnM6IFtcbiAgICBcImJsYWRlLnBocFwiXG4gIF1cblxuICBvcHRpb25zOiBbXVxufVxuIl19
