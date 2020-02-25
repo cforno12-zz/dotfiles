cb3405e10fb75f31dbb0c7daf48ca9716248b271
Cris Forno <cforno12@linux.vnet.ibm.com>
2020-02-25 12:35:23 -0600

    syncing misc emacs files

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -1,2 +1,13 @@
-((magit-diff
-  ("--no-ext-diff")))
+((magit-branch nil)
+ (magit-commit
+  ("--signoff")
+  nil)
+ (magit-diff
+  ("--no-ext-diff"))
+ (magit-dispatch nil)
+ (magit-patch nil)
+ (magit-patch-apply nil)
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-rebase nil)
+ (magit-reset nil))
